a955aaedf30660b14a6e0ad0f47987b495de5ceb
Philippe De Pauw - Waterschoot <phildp@arteveldehs.be>
2023-08-28 11:55:39 +0200

    Initial commit

---
+++ week5/tips/index.js
@@ -0,0 +1,36 @@
+const words = [
+  'Philippe', 
+  'De Pauw',
+  'Waterschoot',
+  'Spitaalpoortstraat',
+  'Zwijnaardsesteenweg',
+  'Bergstraatwasje',
+  '',
+];
+
+function generateTabs(n) {
+  let str = ''
+  for (let i = 0; i < n; i++) {
+    str += '\t';
+  }
+  return str;
+}
+
+function generateTabsForWord(word, amountOfChars) {
+  const n = Math.ceil((amountOfChars - word.length) / 8);
+  console.log(n);
+  let str = ''
+  for (let i = 0; i < n; i++) {
+    str += '\t';
+  }
+  return str;
+}
+
+let output = '';
+for (let i = 0; i < words.length; i++) {
+  output += words[i] + generateTabsForWord(words[i], 32) + 'Next text after tabs' + '\n';
+}
+console.log(output);
+
+const h = 24* Math.tan(2 * Math.PI * 45 / 360);
+console.log(h);
