868c0b5c027648c12a3ea9f50542a22ce478b2bc
Philippe De Pauw - Waterschoot <phildp@MACINTOSH.local>
2023-09-20 11:52:02 +0200

    Added a basic example of JavaScript Syntax and Grammer in Week 01

---
+++ week1/basis/basics_01.js
@@ -0,0 +1,54 @@
+console.log("Hello World");
+console.log("Greetings Earthlings");
+
+// This a one line comment
+/*
+Programming 1: Essentials
+Week: 1
+Date: 20-09-2023
+==========================
+Basics
+Grammer
+Syntax
+*/
+pi = 3.14;
+console.log(pi);
+firstName = "Philippe";
+console.log(firstName);
+dayOfBirth = "12/12/1745";
+firstname = "Willy";
+
+// Declaratie (En. declaration)
+
+// Bij een const variabele moet je direct een waarde aan toekennen
+const lastName = "De Pauw - Waterschoot";
+console.log(lastName);
+
+// Bij een let variabele kan je na toekenning de waarde veranderen
+let age = 367;
+age = 369;
+console.log(age);
+
+// Enkel declareren
+let data;
+console.log(data);
+// Informatie toekennen aan een variabele (En. assignment)
+data = "Barbie";
+
+// Built-in basic datatypes
+// String
+const nickName = "drdynscript";
+const email = 'philippe.depauw@arteveldehs.be';
+let quoteOfTheDay = 'Quote of the day "Learning by doing"';
+quoteOfTheDay = "Quote of the day 'Learning by doing'";
+quoteOfTheDay = 'Quote of the day \'Learning by doing\''; // Backslash escaping \
+console.log(quoteOfTheDay);
+
+// Strings met elkaar verbinden via + teken
+const contact = 'My nickname is ' + nickName + ', my email is ' + email;
+console.log(contact);
+
+// Number
+let salary = 4100;
+salary = salary + 1000;
+console.log(salary);
