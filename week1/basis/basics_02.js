1fedbd83cddba12d2f39aef5001f0ffd17eab96b
Philippe De Pauw - Waterschoot <phildp@MACINTOSH.local>
2023-09-22 14:23:51 +0200

    Added if, else if, else and switch case

--- week1/basis/basics_02.js
+++ week1/basis/basics_02.js
@@ -40,3 +40,46 @@ v = x || y; // true
 v = !x; // false
 // Combinatie van meer dan twee condities
 v = x && !y && x === 3;
+
+let isGameOver = true;
+if (isGameOver) {
+  // B
+  console.log('You can start a new game!');
+} else {
+  // C
+  console.log(`You are playing now :)`);
+}
+
+let isALady = false;
+if (!isALady) {
+  console.log('I am a nice guy.');
+} else {
+  console.log('I am a very nice barbie girl.');
+}
+let msg = isALady ? 'I am a very nice barbie girl.' : 'I am a nice guy.';
+console.log(msg);
+
+// Toets toegangscode: b4x3b
+
+let gender;
+gender = 1;
+if (gender == 0) {
+  msg = 'Gender is not know!';
+} else if (gender == 1) {
+  msg = 'Your are a male!';
+} else if (gender == 2) {
+  msg = 'You are a female!';
+} else {
+  msg = 'Gender is not specified';
+}
+console.log(msg);
+
+// Switch / Case
+gender = 9;
+switch (gender) {
+  case 0: msg = 'Gender is not know!'; break;
+  case 1: msg = 'Your are a male!'; break;
+  case 2: msg = 'You are a female!'; break;
+  default: msg = 'Gender is not specified!'; break;
+}
+console.log(msg);
