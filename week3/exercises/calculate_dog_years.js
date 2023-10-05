00f7817bcdaa032a54322cce4d014efb90952de3
Philippe De Pauw - Waterschoot <phildp@MACINTOSH.local>
2023-10-05 16:16:43 +0200

    Added exercise Dog Years

--- week3/exercises/calculate_dog_years.js
+++ week3/exercises/calculate_dog_years.js
@@ -1,9 +1,18 @@
 function calculateDogYears(dogInHumanYears) {
   return dogInHumanYears * 7;
 }
+
+function calculateHumanYears(dogInDogYears) {
+  return dogInDogYears / 7;
+}
+
 let dogInDogYears = calculateDogYears(15);
 let output = `Your dog is ${dogInDogYears} years old in dog years!`;
 console.log(output);
 dogInDogYears = calculateDogYears(24);
 output = `Your dog is ${dogInDogYears} years old in dog years!`;
+console.log(output);
+
+let dogInHumanYears = calculateHumanYears(104);
+output = `Your dog is ${dogInHumanYears} years old in human years!`;
 console.log(output);
