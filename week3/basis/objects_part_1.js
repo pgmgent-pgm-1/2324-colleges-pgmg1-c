7d14cecc42a515f25b4c208a4981db40b8259137
Philippe De Pauw - Waterschoot <phildp@MACINTOSH.local>
2023-10-05 14:20:12 +0200

    Added object (deel 1)

--- week3/basis/objects_part_1.js
+++ week3/basis/objects_part_1.js
@@ -19,4 +19,21 @@ const person = {
     return 'The person is eating!';
   }
 };
+person.hairColor = 'Pink';
+person.firstName = 'Philippe';
+person.lastName = 'De Pauw - Waterschoot';
 console.log(person);
+const h = person.height;
+console.log(`The height of the person ${person.firstName} ${person.lastName} is ${h}`);
+person['3tier'] = 'Presentation, Logic and Data Tier'; // Associatieve array
+person['cool shit'] = 'This person is the real slim shady';
+// Call the actions
+let output = person.jump();
+console.log(output);
+output = person.eat();
+console.log(output);
+
+// New Loop => Yiha => for...in => doorlopen van alle eigenschappen van een object
+for (let prop in person) {
+  console.log(`The prop ${prop} with value ${person[prop]}`);
+}
